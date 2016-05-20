a5c913231394ed89599b86d112feede110b74c5f
melikesofta <msofta@ku.edu.tr>
2016-05-20 18:03:32 +0300

    initial comments

--- kufs.cpp
+++ kufs.cpp
@@ -1,1 +1,5 @@
-//kufs
+//Written by Meric Melike Softa and Firtina Kucuk on 20.05.2016
+//for the 3rd class project of Comp304 - Operating Systems
+
+//necessary methods for the project are implemented
+//here on top of those implemented in kufs.h

bacb4dc19e2ba822293ba3180d5103fb6d5a141f
melikesofta <msofta@ku.edu.tr>
2016-05-20 18:51:37 +0300

    main module

--- main.c
+++
@@ -1,6 +0,0 @@
-//Written by Meric Melike Softa and Firtina Kucuk on 20.05.2016
-//for the 3rd class project of Comp304 - Operating Systems
-
-//The following link was used as a tutorial and some of the
-//codes we use might be based on that:
-//http://kukuruku.co/hub/nix/writing-a-file-system-in-linux-kernel
